d16ead83ea11cb0dd4d7068d4fc4cd7aa4a9ce9a
claimy17 <sebmoc17@gmail.com>
2026-07-11 15:27:36 +0200

    Update original size in siri_ios27v2.py

--- siri_ios27v2.py
+++ siri_ios27v2.py
@@ -35,7 +35,7 @@ def check_integrity():
   
     try:
         dimensione_corrente = os.path.getsize(file_corrente)
-        dimensione_originale = 36329   
+        dimensione_originale = 36331   
         if dimensione_corrente != dimensione_originale:
             codice_richiesto = "21092008"
     except:
